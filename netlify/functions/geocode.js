87ac3495732475ad8a9ba7ad4be101324f0003c5
akabong <akabongee@gmail.com>
2026-01-07 13:42:53 +0900

    edit geocode function

--- netlify/functions/geocode.js
+++ netlify/functions/geocode.js
@@ -24,12 +24,20 @@ export async function handler(event) {
         if (!res.ok) {
             return {statusCode: res.status, body: JSON.stringify({error: 'nominatim error'})};
         }
-        const data = await res.json();
-        return {
-            statusCode: 200,
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        };
+        // const item = data[0];
+        // console.log(`geocodeDirect:`, data);
+        // return {
+        //     lat: parseFloat(item.lat),
+        //     lon: parseFloat(item.lon),
+        //     displayName: String(item.display_name ?? ''),
+        // };
+
+        const results = data.map((item) => ({
+            lat: parseFloat(item.lat),
+            lon: parseFloat(item.lon),
+            displayName: String(item.display_name ?? ''),
+        }));
+        return results;
     } catch (e) {
         return {statusCode: 500, body: JSON.stringify({error: e.message})};
     }
